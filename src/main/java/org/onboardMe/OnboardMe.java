6f917f8c67fe2fdbaf33c6229cd9306b44c5c6af
WIN-56RI7ET5VE2\ornel <ornella_sofia@hotmail.com>
2025-04-22 22:39:49 -0300

    Added dependencies and git ignore

--- src/main/java/org/onboardMe/OnboardMe.java
+++ src/main/java/org/onboardMe/OnboardMe.java
@@ -1,8 +1,8 @@
-package org.proyectoFinal;
+package org.onboardMe;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class OnboardMe {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
